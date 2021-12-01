bcf3c01427037f33e52545e9bf641cf22e603d2f
carme <carmecodina.p@gmail.com>
2021-12-01 12:25:26 +0100

    exercici 1

--- src/pages/Starships.js
+++ src/pages/Starships.js
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Layout from "../components/Layout";
 
 const Starships = () => {
+    const [ships, setShips] = useState([]);
+    useEffect(() => {
+        axios
+            .get("https://swapi.dev/api/starships/")
+            .then((res) => setShips(res.data.results));
+    }, []);
     return (
         <Layout>
-            <p>Aquí va la llista de naus</p>
+            <ul>
+                {ships.map((ship) => (
+                    <li>
+                        {ship.name}
+                        <br />
+                        {ship.model}
+                    </li>
+                ))}
+            </ul>
         </Layout>
     );
 };
